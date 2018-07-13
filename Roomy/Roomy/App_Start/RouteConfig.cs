cea1d6d70090c8e3b43c5bc89a04eb874d8ebe74
Blandine <Adminl@EMER05>
2018-07-13 16:43:10 +0200

    cours 1307

--- Roomy/Roomy/App_Start/RouteConfig.cs
+++ Roomy/Roomy/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Roomy
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AboutRoute",
+                url: "a-propos",
+                defaults: new{Controller="Home", action = "About"});
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Roomy/Roomy/Controllers/TestsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<string> prenoms = new List<string>
+            {
+                "Gary", "Antoine", "Thomas"
+            };
+
+            return View(prenoms);
+        }
+    }
+}
