8ee7c81edd1f09c84184885ec552c8271f717eb7
Jennifer Martin <jennifer_martin@outlook.fr>
2018-07-16 14:47:56 +0200

    save

---
+++ Roomy/Roomy/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string Lastname)
+        {
+            return View();
+        }
+    }
+}
---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        [Display]
+        public string Lastname { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Mail { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmedPassword { get; set; }
+    }
+}
